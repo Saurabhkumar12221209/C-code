36b9f1d03154d4021ca3bafd3e92c5f99d467319
Saurabhkumar12221209 <119564399+Saurabhkumar12221209@users.noreply.github.com>
2023-09-13 12:49:42 +0530

    arithmetic operator

---
+++ aritmetic opr.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=4,b=6;
+    cout<<"Operators in c++:"<<endl;
+    cout<<"Following are the Arithmetic operators in C++"<<endl;
+    //Arithmetic operators
+    cout<<"The value of a+b is"<<a+b<<endl;
+    cout<<"The value of a-b is"<<a-b<<endl;
+    cout<<"The value of a*b is"<<a*b<<endl;
+    cout<<"The value of a/b is"<<a/b<<endl;
+    cout<<"The value of a++ is"<<a++<<endl;
+    cout<<"The value of a-- is"<<a--<<endl;
+    cout<<"The value of ++a is"<<++a<<endl;
+    cout<<"The value of ++a is"<<--a<<endl;
+    cout<<endl;
+    //Assignment operators--> used to assign value to variables
+    // int a=3,b=9;
+    // char d='d';
+    
+    // comparison operators
+    cout<<"following are the comparison operators in c++"<<endl;
+    cout<<"the value of a==b is"<<(a==b)<<endl;
+    cout<<"the value of a!=b is"<<(a!=b)<<endl;
+    cout<<"the value of a<=b is"<<(a<=b)<<endl;
+    cout<<"the value of a>=b is"<<(a>=b)<<endl;
+    cout<<"the value of a>b is"<<(a>b)<<endl;
+    cout<<"the value of a<b is"<<(a<b)<<endl;
+    cout<<endl;
+    
+    //logical operators
+    cout<<"following are the logical operators in c++"<<endl;
+    cout<<"the value of this logical and operator ((a==b) && (a<b)) is:"<<((a==b) && (a<b))<<endl;
+    cout<<"the value of this logical or operator ((a==b) || (a<b)) is:"<<((a==b) || (a<b))<<endl;
+    
+    return 0;
+
+}
