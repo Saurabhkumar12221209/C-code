973d029d0d0c9a297202dcfb1ceed425a6643aa9
Saurabhkumar12221209 <119564399+Saurabhkumar12221209@users.noreply.github.com>
2023-09-13 12:50:21 +0530

    variables and comments

---
+++ variables and comments.cpp
@@ -0,0 +1,29 @@
+////#include<iostream>
+////using namespace std;
+////int main(){
+////	 int sum =7;
+////	 cout<<"hello saurabh"<<sum;
+////	return 0;
+////}
+//
+/*#include<iostream>
+using namespace std;
+int glo = 6;
+void sum(){
+	int a;
+	cout<<glo;
+}
+
+int main(){
+	int glo =9;
+	glo=79;
+	int a=4,b=7;
+	char c = 'c';
+	bool is_true= true;
+	bool is_false= false;
+//	cout<<"the value of a is"<<a<<"\n.the value of b is"<<b;
+//	cout<<"\n the value of c is:"<<c;
+cout<<glo<<is_true<<is_false;
+	return 0;
+}*/
+ 
